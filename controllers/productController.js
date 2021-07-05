8536b240c6c325c9677eb3a838b0be2b2a2248bc
Matthew Houston <mhouston4994@gmail.com>
2021-07-05 11:14:41 -0700

    added in user reg/login routes/controllers

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,7 +1,28 @@
 import asyncHandler from "express-async-handler";
 import Product from "../models/productModel.js";
 
+// @desc Fetch all Products
+// @route GET /api/products
+// @access Public
 const getProducts = asyncHandler(async (req, res) => {
 	const products = await Product.find({});
 	res.json(products);
 });
+
+// Fetch single Products
+// GET /api/products/:id
+// Public
+const getProductsById = asyncHandler(async (req, res) => {
+	const product = await Product.findById(req.params.id);
+	if (product) {
+		res.json(product);
+	} else {
+		res.status(404);
+		throw new Error("Product not found");
+	}
+})
+
+export {
+	getProducts,
+	getProductsById
+}
--- server.js
+++ server.js
@@ -4,16 +4,20 @@ import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 import connectDB from './config/db.js'
 
 import productRoutes from './routes/productRoutes.js'
+import userRoutes from './routes/userRoutes.js'
 const app = express();
 dotenv.config();
 
 connectDB();
+
+app.use(express.json())
 
 app.get('/', (req,res) => {
     res.send('API is running...')
 })
 
 app.use('/api/products', productRoutes)
+app.use('/api/users', userRoutes)
 
 app.use(notFound)
 
